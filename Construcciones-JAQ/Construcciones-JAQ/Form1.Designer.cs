92634e0bd72cb3cc8fb94dcb0a92022f8e3aac00
jduquinones <eduardoqm23@hotmail.com>
2019-08-29 22:31:56 -0500

    v1.0.2

---
+++ Construcciones-JAQ/Construcciones-JAQ/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Construcciones_JAQ
+{
+    partial class FormRegistroPersonal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardarPersonal = new System.Windows.Forms.Button();
+            this.txtCedulaP = new System.Windows.Forms.TextBox();
+            this.txtTelefonoP = new System.Windows.Forms.TextBox();
+            this.txtSalarioP = new System.Windows.Forms.TextBox();
+            this.txtApellidoP = new System.Windows.Forms.TextBox();
+            this.txtNombreP = new System.Windows.Forms.TextBox();
+            this.btnAgregarP = new System.Windows.Forms.Button();
+            this.txtCorreoP = new System.Windows.Forms.TextBox();
+            this.txtDireccionP = new System.Windows.Forms.TextBox();
+            this.labelNombres = new System.Windows.Forms.Label();
+            this.labelCedula = new System.Windows.Forms.Label();
+            this.labelDirrecion = new System.Windows.Forms.Label();
+            this.labelCorreo = new System.Windows.Forms.Label();
+            this.labelSalario = new System.Windows.Forms.Label();
+            this.labelTelefono = new System.Windows.Forms.Label();
+            this.labelApellidos = new System.Windows.Forms.Label();
+            this.dtgvPersonal = new System.Windows.Forms.DataGridView();
+            this.CedulaP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ApellidoP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelefonoP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SalarioP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CorreoP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DireccionP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.labelBuscar = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvPersonal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardarPersonal
+            // 
+            this.btnGuardarPersonal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGuardarPersonal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarPersonal.Location = new System.Drawing.Point(627, 182);
+            this.btnGuardarPersonal.Name = "btnGuardarPersonal";
+            this.btnGuardarPersonal.Size = new System.Drawing.Size(201, 63);
+            this.btnGuardarPersonal.TabIndex = 4;
+            this.btnGuardarPersonal.Text = "Guardar Registro";
+            this.btnGuardarPersonal.UseVisualStyleBackColor = false;
+            // 
+            // txtCedulaP
+            // 
+            this.txtCedulaP.Location = new System.Drawing.Point(126, 38);
+            this.txtCedulaP.Name = "txtCedulaP";
+            this.txtCedulaP.Size = new System.Drawing.Size(152, 20);
+            this.txtCedulaP.TabIndex = 11;
+            // 
+            // txtTelefonoP
+            // 
+            this.txtTelefonoP.Location = new System.Drawing.Point(433, 38);
+            this.txtTelefonoP.Name = "txtTelefonoP";
+            this.txtTelefonoP.Size = new System.Drawing.Size(152, 20);
+            this.txtTelefonoP.TabIndex = 13;
+            // 
+            // txtSalarioP
+            // 
+            this.txtSalarioP.Location = new System.Drawing.Point(433, 85);
+            this.txtSalarioP.Name = "txtSalarioP";
+            this.txtSalarioP.Size = new System.Drawing.Size(152, 20);
+            this.txtSalarioP.TabIndex = 14;
+            // 
+            // txtApellidoP
+            // 
+            this.txtApellidoP.Location = new System.Drawing.Point(126, 127);
+            this.txtApellidoP.Name = "txtApellidoP";
+            this.txtApellidoP.Size = new System.Drawing.Size(152, 20);
+            this.txtApellidoP.TabIndex = 15;
+            // 
+            // txtNombreP
+            // 
+            this.txtNombreP.Location = new System.Drawing.Point(126, 85);
+            this.txtNombreP.Name = "txtNombreP";
+            this.txtNombreP.Size = new System.Drawing.Size(152, 20);
+            this.txtNombreP.TabIndex = 16;
+            // 
+            // btnAgregarP
+            // 
+            this.btnAgregarP.Location = new System.Drawing.Point(695, 31);
+            this.btnAgregarP.Name = "btnAgregarP";
+            this.btnAgregarP.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarP.TabIndex = 17;
+            this.btnAgregarP.Text = "Agregar";
+            this.btnAgregarP.UseVisualStyleBackColor = true;
+            this.btnAgregarP.Click += new System.EventHandler(this.btnAgregarP_Click);
+            // 
+            // txtCorreoP
+            // 
+            this.txtCorreoP.Location = new System.Drawing.Point(433, 127);
+            this.txtCorreoP.Name = "txtCorreoP";
+            this.txtCorreoP.Size = new System.Drawing.Size(152, 20);
+            this.txtCorreoP.TabIndex = 19;
+            // 
+            // txtDireccionP
+            // 
+            this.txtDireccionP.Location = new System.Drawing.Point(126, 175);
+            this.txtDireccionP.Name = "txtDireccionP";
+            this.txtDireccionP.Size = new System.Drawing.Size(152, 20);
+            this.txtDireccionP.TabIndex = 21;
+            // 
+            // labelNombres
+            // 
+            this.labelNombres.AutoSize = true;
+            this.labelNombres.Location = new System.Drawing.Point(51, 88);
+            this.labelNombres.Name = "labelNombres";
+            this.labelNombres.Size = new System.Drawing.Size(49, 13);
+            this.labelNombres.TabIndex = 22;
+            this.labelNombres.Text = "Nombres";
+            // 
+            // labelCedula
+            // 
+            this.labelCedula.AutoSize = true;
+            this.labelCedula.Location = new System.Drawing.Point(51, 41);
+            this.labelCedula.Name = "labelCedula";
+            this.labelCedula.Size = new System.Drawing.Size(40, 13);
+            this.labelCedula.TabIndex = 23;
+            this.labelCedula.Text = "Cedula";
+            // 
+            // labelDirrecion
+            // 
+            this.labelDirrecion.AutoSize = true;
+            this.labelDirrecion.Location = new System.Drawing.Point(51, 182);
+            this.labelDirrecion.Name = "labelDirrecion";
+            this.labelDirrecion.Size = new System.Drawing.Size(49, 13);
+            this.labelDirrecion.TabIndex = 27;
+            this.labelDirrecion.Text = "Dirrecion";
+            // 
+            // labelCorreo
+            // 
+            this.labelCorreo.AutoSize = true;
+            this.labelCorreo.Location = new System.Drawing.Point(354, 135);
+            this.labelCorreo.Name = "labelCorreo";
+            this.labelCorreo.Size = new System.Drawing.Size(38, 13);
+            this.labelCorreo.TabIndex = 28;
+            this.labelCorreo.Text = "Correo";
+            // 
+            // labelSalario
+            // 
+            this.labelSalario.AutoSize = true;
+            this.labelSalario.Location = new System.Drawing.Point(354, 89);
+            this.labelSalario.Name = "labelSalario";
+            this.labelSalario.Size = new System.Drawing.Size(39, 13);
+            this.labelSalario.TabIndex = 29;
+            this.labelSalario.Text = "Salario";
+            // 
+            // labelTelefono
+            // 
+            this.labelTelefono.AutoSize = true;
+            this.labelTelefono.Location = new System.Drawing.Point(354, 38);
+            this.labelTelefono.Name = "labelTelefono";
+            this.labelTelefono.Size = new System.Drawing.Size(49, 13);
+            this.labelTelefono.TabIndex = 30;
+            this.labelTelefono.Text = "Telefono";
+            // 
+            // labelApellidos
+            // 
+            this.labelApellidos.AutoSize = true;
+            this.labelApellidos.Location = new System.Drawing.Point(51, 134);
+            this.labelApellidos.Name = "labelApellidos";
+            this.labelApellidos.Size = new System.Drawing.Size(49, 13);
+            this.labelApellidos.TabIndex = 31;
+            this.labelApellidos.Text = "Apellidos";
+            // 
+            // dtgvPersonal
+            // 
+            this.dtgvPersonal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvPersonal.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CedulaP,
+            this.NombreP,
+            this.ApellidoP,
+            this.TelefonoP,
+            this.SalarioP,
+            this.CorreoP,
+            this.DireccionP});
+            this.dtgvPersonal.Location = new System.Drawing.Point(54, 273);
+            this.dtgvPersonal.Name = "dtgvPersonal";
+            this.dtgvPersonal.Size = new System.Drawing.Size(743, 213);
+            this.dtgvPersonal.TabIndex = 33;
+            // 
+            // CedulaP
+            // 
+            this.CedulaP.HeaderText = "Cedula";
+            this.CedulaP.Name = "CedulaP";
+            // 
+            // NombreP
+            // 
+            this.NombreP.HeaderText = "Nombre";
+            this.NombreP.Name = "NombreP";
+            // 
+            // ApellidoP
+            // 
+            this.ApellidoP.HeaderText = "Apellido";
+            this.ApellidoP.Name = "ApellidoP";
+            // 
+            // TelefonoP
+            // 
+            this.TelefonoP.HeaderText = "Telefono";
+            this.TelefonoP.Name = "TelefonoP";
+            // 
+            // SalarioP
+            // 
+            this.SalarioP.HeaderText = "Salario";
+            this.SalarioP.Name = "SalarioP";
+            // 
+            // CorreoP
+            // 
+            this.CorreoP.HeaderText = "Correo";
+            this.CorreoP.Name = "CorreoP";
+            // 
+            // DireccionP
+            // 
+            this.DireccionP.HeaderText = "Direccion";
+            this.DireccionP.Name = "DireccionP";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(695, 78);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 34;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(695, 134);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 35;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // labelBuscar
+            // 
+            this.labelBuscar.AutoSize = true;
+            this.labelBuscar.Location = new System.Drawing.Point(60, 228);
+            this.labelBuscar.Name = "labelBuscar";
+            this.labelBuscar.Size = new System.Drawing.Size(40, 13);
+            this.labelBuscar.TabIndex = 36;
+            this.labelBuscar.Text = "Buscar";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(126, 225);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(459, 20);
+            this.txtBuscar.TabIndex = 37;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // FormRegistroPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 498);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.labelBuscar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dtgvPersonal);
+            this.Controls.Add(this.labelApellidos);
+            this.Controls.Add(this.labelTelefono);
+            this.Controls.Add(this.labelSalario);
+            this.Controls.Add(this.labelCorreo);
+            this.Controls.Add(this.labelDirrecion);
+            this.Controls.Add(this.labelCedula);
+            this.Controls.Add(this.labelNombres);
+            this.Controls.Add(this.txtDireccionP);
+            this.Controls.Add(this.txtCorreoP);
+            this.Controls.Add(this.btnAgregarP);
+            this.Controls.Add(this.txtNombreP);
+            this.Controls.Add(this.txtApellidoP);
+            this.Controls.Add(this.txtSalarioP);
+            this.Controls.Add(this.txtTelefonoP);
+            this.Controls.Add(this.txtCedulaP);
+            this.Controls.Add(this.btnGuardarPersonal);
+            this.Name = "FormRegistroPersonal";
+            this.Text = "Registro Personal";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvPersonal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGuardarPersonal;
+        private System.Windows.Forms.TextBox txtCedulaP;
+        private System.Windows.Forms.TextBox txtTelefonoP;
+        private System.Windows.Forms.TextBox txtSalarioP;
+        private System.Windows.Forms.TextBox txtApellidoP;
+        private System.Windows.Forms.TextBox txtNombreP;
+        private System.Windows.Forms.Button btnAgregarP;
+        private System.Windows.Forms.TextBox txtCorreoP;
+        private System.Windows.Forms.TextBox txtDireccionP;
+        private System.Windows.Forms.Label labelNombres;
+        private System.Windows.Forms.Label labelCedula;
+        private System.Windows.Forms.Label labelDirrecion;
+        private System.Windows.Forms.Label labelCorreo;
+        private System.Windows.Forms.Label labelSalario;
+        private System.Windows.Forms.Label labelTelefono;
+        private System.Windows.Forms.Label labelApellidos;
+        private System.Windows.Forms.DataGridView dtgvPersonal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CedulaP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ApellidoP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TelefonoP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SalarioP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CorreoP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DireccionP;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Label labelBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
---
+++ Construcciones-JAQ/Construcciones-JAQ/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Construcciones_JAQ
+{
+    public partial class FormRegistroPersonal : Form
+    {
+        public FormRegistroPersonal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregarP_Click(object sender, EventArgs e)
+        {
+            //Adicionar NUevo Renglon
+
+            int n = dtgvPersonal.Rows.Add();
+
+            //ingresar informacion
+            dtgvPersonal.Rows[n].Cells[0].Value = txtCedulaP.Text;
+            dtgvPersonal.Rows[n].Cells[1].Value = txtNombreP.Text;
+            dtgvPersonal.Rows[n].Cells[2].Value = txtApellidoP.Text;
+            dtgvPersonal.Rows[n].Cells[3].Value = txtTelefonoP.Text;
+            dtgvPersonal.Rows[n].Cells[4].Value = txtSalarioP.Text;
+            dtgvPersonal.Rows[n].Cells[5].Value = txtCorreoP.Text;
+            dtgvPersonal.Rows[n].Cells[6].Value = txtDireccionP.Text;
+
+            //limpiar los textbox
+            txtCedulaP.Text = "";
+            txtNombreP.Text = "";
+            txtApellidoP.Text = "";
+            txtTelefonoP.Text = "";
+            txtSalarioP.Text = "";
+            txtCorreoP.Text = "";
+            txtDireccionP.Text = "";
+        }
+       
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            if(txtBuscar.Text == "")
+            {
+                dtgvPersonal.CurrentCell = null;
+
+                foreach (DataGridViewRow r in dtgvPersonal.Rows)
+                {
+                    r.Visible = false;
+
+                }
+                foreach (DataGridViewRow r in dtgvPersonal.Rows)
+                {
+                    foreach (DataGridViewCell c in r.Cells)
+                    {
+                        if ((c.Value.ToString().ToUpper()).IndexOf(txtBuscar.Text.ToUpper())== 0)
+                        {
+                            r.Visible = true;
+                            break;
+                        }
+
+                    }
+
+                }
+            }
+            else
+            {
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            dtgvPersonal.Rows.Remove(dtgvPersonal.CurrentRow);
+        }
+    }
+}

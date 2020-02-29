4bab01cf5394e927d692d8683449529077a888dd
Jquinones <42759236+jduquinones@users.noreply.github.com>
2020-02-29 03:05:58 -0500

    Revert "Actulizacion clase usuario personal"

--- Construcciones-JAQ/Construcciones-JAQ/Form1.cs
+++ Construcciones-JAQ/Construcciones-JAQ/Form1.cs
@@ -66,15 +66,5 @@ namespace Construcciones_JAQ
             txtCorreoP.Text = "";
             txtDireccionP.Text = "";
         }
-
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
